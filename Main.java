9e7558c1e7722c48286ab70405494a8b776c38a6
Edelenad <89814947+Edelenad@users.noreply.github.com>
2021-12-01 04:55:36 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+// @ Andrew Edelen and Evan Naylor
+
+
+class Main {
+  public static void main(String[] args) {
+    Sugar tree = new Sugar();
+    tree.setShape("tree");
+
+    String treeShape = tree.getShape();
+    tree.cutShape(treeShape,10);
+    tree.bake(350,12);
+    tree.decorations();
+    Sugar aCookie = new Sugar();
+    aCookie.decorations();
+  }
+}
